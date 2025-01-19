0e3f5335ac4437f9c4b56a430a13e8150d3da0f0
duck59uet <ducnv1911@gmail.com>
2025-01-19 12:00:14 +0700

    fix

--- src/modules/service/service.service.ts
+++ src/modules/service/service.service.ts
@@ -19,7 +19,7 @@ export class ServiceService {
 
   async getService(categoryId: number): Promise<ResponseDto<any>> {
     try {
-      const data = await this.serviceRepo.repo.find({ where: { status: ServiceStatus.ACTIVE, categoryId }, order: { name: 'ASC' } });
+      const data = await this.serviceRepo.repo.find({ where: { status: ServiceStatus.ACTIVE, categoryId }, order: { id: 'ASC' } });
       return ResponseDto.response(ErrorMap.SUCCESSFUL, data);
     } catch (error) {
       return ResponseDto.responseError(ServiceRepository.name, error);
