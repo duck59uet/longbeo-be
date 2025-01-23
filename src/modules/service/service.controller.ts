49125fcd7b24e80ddc517f909c948773fe12b43f
duck59uet <ducnv1911@gmail.com>
2025-01-23 14:58:37 +0700

    fix

--- src/modules/service/service.controller.ts
+++ src/modules/service/service.controller.ts
@@ -3,10 +3,9 @@ import { ApiTags } from '@nestjs/swagger';
 import { CONTROLLER_CONSTANTS } from '../../common/constants/api.constant';
 import { ServiceService } from './service.service';
 import {
-  CommonAuthPost,
+  CommonAuthGet,
   CommonAuthPut,
   CommonGet,
-  Roles,
 } from '../../decorators/common.decorator';
 import { ResponseDto } from '../../common/dtos';
 import { UpdateServiceDto } from './dto/update-service.req';
@@ -35,6 +34,22 @@ export class ServiceController {
     return this.serviceService.getService(categoryId);
   }
 
+  @CommonAuthGet({
+    url: 'all/:categoryId',
+    summary: 'Admin get all services',
+    apiOkResponseOptions: {
+      status: 200,
+      type: ResponseDto,
+      description: 'Admin get all services',
+      schema: {},
+    },
+  })
+  async getAdminService(
+    @Param('categoryId') categoryId: number,
+  ): Promise<ResponseDto<any>> {
+    return this.serviceService.getAdminService(categoryId);
+  }
+
   @CommonAuthPut({
     url: ':id',
     summary: 'Update service',
@@ -45,11 +60,26 @@ export class ServiceController {
       schema: {},
     },
   })
-  @Roles(UserRole.ADMIN)
   async updateService(
     @Param('id') id: number,
     @Body() body: UpdateServiceDto,
   ): Promise<ResponseDto<any>> {
     return this.serviceService.updateService(id, body);
+  }
+
+  @CommonAuthPut({
+    url: 'status/:id',
+    summary: 'Update service status',
+    apiOkResponseOptions: {
+      status: 200,
+      type: ResponseDto,
+      description: 'Update service status',
+      schema: {},
+    },
+  })
+  async updateServiceStatus(
+    @Param('id') id: number,
+  ): Promise<ResponseDto<any>> {
+    return this.serviceService.updateServiceStatus(id);
   }
 }
--- src/modules/service/service.service.ts
+++ src/modules/service/service.service.ts
@@ -3,7 +3,7 @@ import { ResponseDto } from '../../common/dtos';
 import { ErrorMap } from '../../common/error.map';
 import { ServiceRepository } from './service.repository';
 import { CommonUtil } from '../../utils/common.util';
-import { ServiceStatus } from '../../common/constants/app.constant';
+import { ServiceStatus, UserRole } from '../../common/constants/app.constant';
 import { UpdateServiceDto } from './dto/update-service.req';
 
 @Injectable()
@@ -11,31 +11,85 @@ export class ServiceService {
   private readonly logger = new Logger(ServiceService.name);
   private readonly commonUtil: CommonUtil = new CommonUtil();
 
-  constructor(
-    private serviceRepo: ServiceRepository,
-  ) {
+  constructor(private serviceRepo: ServiceRepository) {
     this.logger.log('============== Constructor Order Service ==============');
   }
 
   async getService(categoryId: number): Promise<ResponseDto<any>> {
     try {
-      const data = await this.serviceRepo.repo.find({ where: { status: ServiceStatus.ACTIVE, categoryId }, order: { id: 'ASC' } });
+      const data = await this.serviceRepo.repo.find({
+        where: { status: ServiceStatus.ACTIVE, categoryId },
+        order: { id: 'ASC' },
+      });
       return ResponseDto.response(ErrorMap.SUCCESSFUL, data);
     } catch (error) {
       return ResponseDto.responseError(ServiceRepository.name, error);
     }
   }
 
-  async updateService(id: number, body: UpdateServiceDto): Promise<ResponseDto<any>> {
+  async getAdminService(categoryId: number): Promise<ResponseDto<any>> {
+    try {
+      const authInfo = this.commonUtil.getAuthInfo();
+      if (authInfo.role === UserRole.USER) {
+        return ResponseDto.responseError(
+          ServiceRepository.name,
+          ErrorMap.PERMISSION_DENIED,
+        );
+      }
+      const data = await this.serviceRepo.repo.find({
+        where: { categoryId },
+        order: { id: 'ASC' },
+      });
+      return ResponseDto.response(ErrorMap.SUCCESSFUL, data);
+    } catch (error) {
+      return ResponseDto.responseError(ServiceRepository.name, error);
+    }
+  }
+
+  async updateServiceStatus(id: number): Promise<ResponseDto<any>> {
+    try {
+      const authInfo = this.commonUtil.getAuthInfo();
+      if (authInfo.role === UserRole.USER) {
+        return ResponseDto.responseError(
+          ServiceRepository.name,
+          ErrorMap.PERMISSION_DENIED,
+        );
+      }
+
+      const service = await this.serviceRepo.repo.findOne({ where: { id } });
+      service.status === ServiceStatus.ACTIVE
+        ? ServiceStatus.INACTIVE
+        : ServiceStatus.ACTIVE;
+      const result = await this.serviceRepo.repo.update(
+        { id },
+        { status: service.status },
+      );
+
+      return ResponseDto.response(ErrorMap.SUCCESSFUL, result);
+    } catch (error) {
+      return ResponseDto.responseError(ServiceRepository.name, error);
+    }
+  }
+
+  async updateService(
+    id: number,
+    body: UpdateServiceDto,
+  ): Promise<ResponseDto<any>> {
     try {
       const service = await this.serviceRepo.repo.findOne({ where: { id } });
       if (!service) {
-        return ResponseDto.responseError(ServiceRepository.name, ErrorMap.SERVICE_NOT_FOUND);
+        return ResponseDto.responseError(
+          ServiceRepository.name,
+          ErrorMap.SERVICE_NOT_FOUND,
+        );
       }
 
-      const { name, sourceAddress, price, rate} = body;
+      const { name, sourceAddress, price, rate } = body;
 
-      await this.serviceRepo.repo.update({ id }, { name, sourceAddress, price, rate });
+      await this.serviceRepo.repo.update(
+        { id },
+        { name, sourceAddress, price, rate },
+      );
       return ResponseDto.response(ErrorMap.SUCCESSFUL, {});
     } catch (error) {
       return ResponseDto.responseError(ServiceRepository.name, error);
